fc8c075c0f18324b5a7d28c0a6378cdcf41bcf96
GlockGao <57064620@qq.com>
2025-05-16 06:11:00 +0000

    rename gemini files and and gemini-pro-2.5

---
+++ vertex/multimodal/gemini/gemini_pro_25_basic.py
@@ -0,0 +1,19 @@
+import os
+from google import genai
+
+gemini_api_key = os.getenv('GEMINI_API_KEY')
+
+if gemini_api_key:
+    print(f"获取到的 GEMINI_API_KEY: {gemini_api_key}")
+else:
+    print("环境变量 'GEMINI_API_KEY' 未设置。")
+
+client = genai.Client(api_key=gemini_api_key)
+
+prompt = "Explain the concept of Occam's Razor and provide a simple, everyday example."
+response = client.models.generate_content(
+    model="gemini-2.5-pro-preview-05-06",
+    contents=prompt
+)
+
+print(response.text)
