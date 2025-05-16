885c2d7d4594a2d77dd3ecad6979797cfbab7c75
GlockGao <57064620@qq.com>
2025-05-16 08:03:05 +0000

    add imagen-3.0 model

---
+++ vertex/image/imagen_3.0_basic.py
@@ -0,0 +1,48 @@
+from google import genai
+from google.genai import types
+from PIL import Image
+from io import BytesIO
+import base64
+from typing import Union
+import os
+from utils.time_utils import timing_decorator
+
+
+gemini_api_key = os.getenv('GEMINI_API_KEY')
+
+if gemini_api_key:
+    print(f"获取到的 GEMINI_API_KEY: {gemini_api_key}")
+else:
+    print("环境变量 'GEMINI_API_KEY' 未设置。")
+
+client = genai.Client(api_key=gemini_api_key)
+
+
+@timing_decorator
+def generate(prompt: str,
+             model: str = "imagen-3.0-generate-002"):
+
+    response = client.models.generate_images(
+        model=model,
+        prompt=prompt,
+        config=types.GenerateImagesConfig(
+            number_of_images=1,
+            aspect_ratio="1:1"
+        )
+    )
+
+    for generated_image in response.generated_images:
+        image = Image.open(BytesIO(generated_image.image.image_bytes))
+        image.save('imagen-native-image.png')
+
+
+def main():
+
+    prompt = '''Hi, can you create a 3d rendered image of a pig 
+                with wings and a top hat flying over a happy 
+                futuristic scifi city with lots of greenery?'''
+    generate(prompt=prompt, model="imagen-3.0-generate-002") 
+
+
+if __name__ == "__main__":
+    main()
