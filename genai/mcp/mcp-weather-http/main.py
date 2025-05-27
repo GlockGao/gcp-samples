a6162eafbb8886c37a6e95e18794ce800350bc23
GlockGao <57064620@qq.com>
2025-05-27 03:24:10 +0000

    initial mcp http stream server , but failed to connect

--- genai/mcp/mcp-weather-http/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from mcp-weather-http!")
-
-
-if __name__ == "__main__":
-    main()
--- genai/mcp/mcp-weather-http/setup.sh
+++ genai/mcp/mcp-weather-http/setup.sh
@@ -5,8 +5,8 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 
 # MCP server
-uv init mcp-weather
-cd mcp-weather
+uv init mcp-weather-http
+cd mcp-weather-http
 uv venv
 source .venv/bin/activate
 
@@ -17,3 +17,11 @@ uv add "mcp[cli]" httpx
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 nvm install 22
+
+# Source directory
+rm -f main.py
+mkdir -p src/mcp_weather_http
+cd src/mcp_weather_http/
+touch __init__.py
+touch __main__.py
+touch server.py
