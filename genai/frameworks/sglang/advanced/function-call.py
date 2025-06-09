80882ee029a6f8b07565b6be59bc79c1d9b9900b
GlockGao <57064620@qq.com>
2025-06-09 08:38:33 +0000

    sglang advanced feature - function-call

--- genai/frameworks/sglang/advanced/function-call.py
+++ genai/frameworks/sglang/advanced/function-call.py
@@ -1,4 +1,14 @@
 
+import openai
+import json
+from sglang.utils import print_highlight
+
+
+host = "localhost"
+host = "10.128.0.28"
+port = 30000
+
+client = openai.Client(base_url=f"http://{host}:{port}/v1", api_key="None")
 
 # Define tools
 tools = [
@@ -30,3 +40,34 @@ tools = [
         },
     }
 ]
+
+def get_messages():
+    return [
+        {
+            "role": "user",
+            "content": "What's the weather like in Boston today? Output a reasoning before act, then use the tools to help you.",
+        }
+    ]
+
+
+messages = get_messages()
+model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+model_name = "Qwen/Qwen2.5-7B"
+model_name = "Qwen/Qwen3-8B"
+
+# Non-streaming mode test
+response_non_stream = client.chat.completions.create(
+    model=model_name,
+    messages=messages,
+    temperature=0,
+    top_p=0.95,
+    max_tokens=1024,
+    stream=False,  # Non-streaming
+    tools=tools,
+)
+print_highlight("Non-stream response:")
+print(response_non_stream)
+print_highlight("==== content ====")
+print(response_non_stream.choices[0].message.content)
+print_highlight("==== tool_calls ====")
+print(response_non_stream.choices[0].message.tool_calls)
